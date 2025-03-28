0df5e8c45c30988610d90494b5e6d7f4e58dc459
LSewell52 <leona.sewell52@buc.blinn.edu>
2025-03-28 00:10:11 -0500

    footer

--- components/js/footer.js
+++ components/js/footer.js
@@ -1,27 +1,35 @@
 class Footer extends HTMLElement {
     constructor() {
-        super();
-        this.innerHTML = `
-            <footer class="footer bg-light text-dark py-3">
-                <div class="container">
-                    <div class="row">
-                        <!-- Left-aligned phone number -->
-                        <div class="col-md-4 text-md-start">
-                            <p>(903) 123-4567</p>
-                        </div>
-
-                        <!-- Center-aligned address -->
-                        <div class="col-md-4 text-md-center">
-                            <p>910 Main Ln, Austin, TX 00000</p>
-                        </div>
-
-                        <!-- Right-aligned email -->
-                        <div class="col-md-4 text-md-end">
-                            <p><a href="mailto:unmadeemail@example.com" class="text-dark">unmadeemail@example.com</a></p>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        `;
+      super();
     }
-}
+  
+
+
+    connectedCallback() {
+
+
+        fetch('components/footer.html')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.text(); // or response.json() if it's a JSON file
+        })
+        .then(data => {
+          console.log('Document content:', data);
+          // Further processing of the document content
+        
+          var globalData = data;
+          this.innerHTML = globalData;
+        
+        })
+        .catch(error => {
+          console.error('There was a problem fetching the document:', error);
+        });
+
+
+      
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
